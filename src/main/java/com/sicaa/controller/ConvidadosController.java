0dd7f15003d4e6b0982ba04af5973c2b4ff954a5
Edmo Felipe Ferreira <edmo@piodecimo.edu.br>
2017-03-24 11:50:31 -0300

    editar

--- src/main/java/com/sicaa/controller/ConvidadosController.java
+++ src/main/java/com/sicaa/controller/ConvidadosController.java
@@ -3,6 +3,7 @@ package com.sicaa.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -42,4 +43,13 @@ public class ConvidadosController {
 		mv.addObject("convidados", todosConvidados);
 		return mv;
 	}
+	
+	@RequestMapping("{id}")
+	public ModelAndView edicao(@PathVariable("id") Convidado convidado){
+		ModelAndView mv = new ModelAndView("CadastroConvidados");
+		mv.addObject(convidado);
+		return mv;
+	}
+	
+	
 }
--- src/main/resources/static/layout/javascripts/algaworks.js
+++ src/main/resources/static/layout/javascripts/algaworks.js
@@ -84,3 +84,20 @@ $(function() {
   $('.js-search-modal-close').bind('click', AW.onSearchModalCloseRequest);
   //$('.js-form-loading').bind('submit', AW.onFormLoadingSubmit);
 });
+
+$('#confirmacaoExclusaoModal').on('show.bs.modal', function(event) {
+	var button = $(event.relatedTarget);
+	
+	var codigoTitulo = button.data('id');
+	var descricaoTitulo = button.data('descricao');
+	
+	var modal = $(this);
+	var form = modal.find('form');
+	var action = form.attr('action');
+	if (!action.endsWith('/')) {
+		action += '/';
+	}
+	form.attr('action', action + codigoTitulo);
+	
+	modal.find('.modal-body span').html('Tem certeza que deseja excluir o título <strong>' + descricaoTitulo + '</strong>?');
+});
