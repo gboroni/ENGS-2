c8c9d82c901da2c11c61a87e4c74843bd4a0cd50
Guilherme Boroni <guilhermeboroni@MacBook-Air-de-Guilherme.local>
2017-04-14 21:35:51 -0300

    list avaliacoes

--- src/main/java/com/sicaa/controller/AvaliacaosController.java
+++ src/main/java/com/sicaa/controller/AvaliacaosController.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.sicaa.model.Apresentacao;
+import com.sicaa.repository.ApresentacaoAlunos;
 import com.sicaa.repository.Apresentacaos;
 import com.sicaa.repository.Avaliacaos;
 
@@ -21,12 +23,30 @@ public class AvaliacaosController {
 	@Autowired
 	private Apresentacaos apresentacaos;
 	
+	@Autowired
+	private ApresentacaoAlunos alunos;
+	
+	
 	
 	@RequestMapping
 	public ModelAndView pesquisar() {
 		List<Apresentacao> todosAvaliacaos = apresentacaos.findAllApresentacaosAvaliadas();
 		ModelAndView mv = new ModelAndView("ListaAvaliacaos");
 		mv.addObject("avaliacaosAvaliadas", todosAvaliacaos);
+		return mv;
+	}
+	
+	@RequestMapping("{id}")
+	public ModelAndView edicao(@PathVariable("id") Integer id) {
+		ModelAndView mv = new ModelAndView("ListaAvaliacaoAlunos");
+		mv.addObject("apalunos",alunos.findAllAlunosByApresentacao(id));
+		return mv;
+	}
+	
+	@RequestMapping("{id}/{id_apresentacao_aluno}")
+	public ModelAndView edicao(@PathVariable("id") Integer id, @PathVariable("id_apresentacao_aluno") Integer id_apresentacao_aluno) {
+		ModelAndView mv = new ModelAndView("ListaAlunoAvaliadores");
+		mv.addObject("avaliadores",avaliacaos.findAllAvaliadoresByApresentacao(id_apresentacao_aluno));
 		return mv;
 	}
 
