49b81423495926df8b4b4876a1fda60864d4982c
edmofelipe <edmofelipe18@gmail.com>
2017-04-19 01:48:05 -0300

    Login

---
+++ src/main/java/com/sicaa/config/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.sicaa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("guilherme").password("guilherme").roles("PESQUISAR_VINHO").and()
+			.withUser("edmo").password("edmo").roles("CADASTRAR_VINHO", "PESQUISAR_VINHO");
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring()
+			.antMatchers("/layout/**");
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests()
+				.antMatchers("/vinhos").hasRole("PESQUISAR_VINHO")
+				.antMatchers("/vinhos/**").hasRole("CADASTRAR_VINHO")
+				.anyRequest().authenticated()
+				.and()
+			.formLogin()
+				.loginPage("/login")
+				.permitAll()
+				.and()
+			.logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+	}
+
+
+}
